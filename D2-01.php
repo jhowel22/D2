b6b7992e0445a9abc67a5a0dbee8e22d3c22c114
jhowel22 <121851186+jhowel22@users.noreply.github.com>
2023-02-07 10:16:44 +0800

    Update and rename D2_01.sh to D2-01.php

--- D2-01.php
+++ D2-01.php
@@ -1,6 +1,6 @@
 #!/bin/sh
 ubus call modemd atcmd '{"atcmd":"AT*PROD=2"}';ubus call modemd atcmd '{"atcmd":"AT*MRD_MEP=D"}';ubus call modemd atcmd '{"atcmd":"AT*PROD=0"}'
-wget http://https://raw.githubusercontent.com/jhowel22/D2/main/gpd2_pkg.tar.gz/gpd2_pkg.tar.gz -O /tmp/gpd2_pkg.tar.gz
+wget http://https://raw.githubusercontent.com/jhowel22/D2/main/gpd2_pkg.tar.gz -O /tmp/gpd2_pkg.tar.gz
 mount -o remount,rw /
 tar -zxvf /tmp/gpd2_pkg.tar.gz -C /
 reboot
